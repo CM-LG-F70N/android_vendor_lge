ec6b7cb83015c83e8070b5be9bd649f164cd14eb
Nikita <nikich340@gmail.com>
2015-12-27 19:16:08 +1000

    Update .mk

--- f70n/f70n-vendor-blobs.mk
+++ f70n/f70n-vendor-blobs.mk
@@ -141,36 +141,41 @@ PRODUCT_COPY_FILES += \
     vendor/lge/f70n/proprietary/lib/liblgftmitem.so:system/lib/liblgftmitem.so \
     vendor/lge/f70n/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
     vendor/lge/f70n/proprietary/lib/hw/camera.msm8226.so:system/lib/hw/camera.msm8226.so \
+    vendor/lge/f70n/proprietary/lib/libHDR.so:system/lib/libHDR.so \
+    vendor/lge/f70n/proprietary/lib/libmm-qcamera.so:system/lib/libmm-qcamera.so \
+    vendor/lge/f70n/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
+    vendor/lge/f70n/proprietary/lib/libmmcamera_hi543.so:system/lib/libmmcamera_hi543.so \
+    vendor/lge/f70n/proprietary/lib/libmmcamera_hi707.so:system/lib/libmmcamera_hi707.so \
+    vendor/lge/f70n/proprietary/lib/libmmcamera_imx111.so:system/lib/libmmcamera_imx111.so \
+    vendor/lge/f70n/proprietary/lib/libmmcamera_imx119.so:system/lib/libmmcamera_imx119.so \
+    vendor/lge/f70n/proprietary/lib/libmmcamera_imx179.so:system/lib/libmmcamera_imx179.so \
+    vendor/lge/f70n/proprietary/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so \
+    vendor/lge/f70n/proprietary/lib/libmmjpeg_interface.so:system/lib/libmmjpeg_interface.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_hi543_common.so:system/lib/libchromatix_hi543_common.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_hi543_default_video.so:system/lib/libchromatix_hi543_default_video.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_hi543_preview.so:system/lib/libchromatix_hi543_preview.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_hi543_snapshot.so:system/lib/libchromatix_hi543_snapshot.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_imx111_common.so:system/lib/libchromatix_imx111_common.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_imx111_default_video.so:system/lib/libchromatix_imx111_default_video.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_imx111_preview.so:system/lib/libchromatix_imx111_preview.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_imx111_snapshot.so:system/lib/libchromatix_imx111_snapshot.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_imx119_common.so:system/lib/libchromatix_imx119_common.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_imx119_default_video.so:system/lib/libchromatix_imx119_default_video.so \
+    vendor/lge/f70n/proprietary/lib/libchromatix_imx119_preview.so:system/lib/libchromatix_imx119_preview.so \
     vendor/lge/f70n/proprietary/lib/libchromatix_imx179_common.so:system/lib/libchromatix_imx179_common.so \
     vendor/lge/f70n/proprietary/lib/libchromatix_imx179_default_video.so:system/lib/libchromatix_imx179_default_video.so \
     vendor/lge/f70n/proprietary/lib/libchromatix_imx179_preview.so:system/lib/libchromatix_imx179_preview.so \
     vendor/lge/f70n/proprietary/lib/libchromatix_imx179_snapshot.so:system/lib/libchromatix_imx179_snapshot.so \
-    vendor/lge/f70n/proprietary/lib/libHDR.so:system/lib/libHDR.so \
-    vendor/lge/f70n/proprietary/lib/libmmcamera_imx179.so:system/lib/libmmcamera_imx179.so \
-    vendor/lge/f70n/proprietary/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so \
     vendor/lge/f70n/proprietary/lib/libmorpho_image_stab31.so:system/lib/libmorpho_image_stab31.so \
     vendor/lge/f70n/proprietary/lib/libmorpho_memory_allocator.so:system/lib/libmorpho_memory_allocator.so \
     vendor/lge/f70n/proprietary/lib/libmorpho_panorama_gp.so:system/lib/libmorpho_panorama_gp.so \
-    vendor/lge/f70n/proprietary/lib/libmorpho_noise_reduction.so:system/lib/libmorpho_noise_reduction.so \
-    vendor/lge/f70n/proprietary/lib/libmmjpeg_interface.so:system/lib/libmmjpeg_interface.so \
-    vendor/lge/f70n/proprietary/lib/libmm-qcamera.so:system/lib/libmm-qcamera.so \
+    vendor/lge/f70n/proprietary/lib/libmorpho_panorama_wa_viewer.so:system/lib/libmorpho_panorama_wa_viewer.so \
     vendor/lge/f70n/proprietary/vendor/lib/liboemcamera.so:system/vendor/lib/liboemcamera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libmmjpeg.so:system/vendor/lib/libmmjpeg.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libjpegdhw.so:system/vendor/lib/libjpegdhw.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libjpegehw.so:system/vendor/lib/libjpegehw.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libqomx_jpegdec.so:system/vendor/lib/libqomx_jpegdec.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libqomx_jpegenc.so:system/vendor/lib/libqomx_jpegenc.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libmmqjpeg_codec.so:system/vendor/lib/libmmqjpeg_codec.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libchromatix_imx119_common.so:system/vendor/lib/libchromatix_imx119_common.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libchromatix_imx119_default_video.so:system/vendor/lib/libchromatix_imx119_default_video.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libchromatix_imx119_preview.so:system/vendor/lib/libchromatix_imx119_preview.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_c2d_module.so:system/vendor/lib/libmmcamera2_c2d_module.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_cpp_module.so:system/vendor/lib/libmmcamera2_cpp_module.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_frame_algorithm.so:system/vendor/lib/libmmcamera2_frame_algorithm.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_iface_modules.so:system/vendor/lib/libmmcamera2_iface_modules.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_imglib_modules.so:system/vendor/lib/libmmcamera2_imglib_modules.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_isp_modules.so:system/vendor/lib/libmmcamera2_isp_modules.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_is.so:system/vendor/lib/libmmcamera2_is.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_pproc_modules.so:system/vendor/lib/libmmcamera2_pproc_modules.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_sensor_modules.so:system/vendor/lib/libmmcamera2_sensor_modules.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera2_stats_algorithm.so:system/vendor/lib/libmmcamera2_stats_algorithm.so \
@@ -180,50 +185,23 @@ PRODUCT_COPY_FILES += \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_faceproc.so:system/vendor/lib/libmmcamera_faceproc.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_hdr_gb_lib.so:system/vendor/lib/libmmcamera_hdr_gb_lib.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_hdr_lib.so:system/vendor/lib/libmmcamera_hdr_lib.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_hi351.so:system/vendor/lib/libmmcamera_hi351.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_hi543_eeprom.so:system/vendor/lib/libmmcamera_hi543_eeprom.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_imglib.so:system/vendor/lib/libmmcamera_imglib.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_imx119.so:system/vendor/lib/libmmcamera_imx119.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_imx111_eeprom.so:system/vendor/lib/libmmcamera_imx111_eeprom.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_imx179_eeprom.so:system/vendor/lib/libmmcamera_imx179_eeprom.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_mt9m114.so:system/vendor/lib/libmmcamera_mt9m114.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_tintless_algo.so:system/vendor/lib/libmmcamera_tintless_algo.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_tintless_bg_pca_algo.so:system/vendor/lib/libmmcamera_tintless_bg_pca_algo.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_tuning.so:system/vendor/lib/libmmcamera_tuning.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmcamera_wavelet_lib.so:system/vendor/lib/libmmcamera_wavelet_lib.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714_camcorder.so:system/vendor/lib/libactuator_dw9714_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714_camera.so:system/vendor/lib/libactuator_dw9714_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714_imx091_camcorder.so:system/vendor/lib/libactuator_dw9714_imx091_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714_imx091_camera.so:system/vendor/lib/libactuator_dw9714_imx091_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714_imx091.so:system/vendor/lib/libactuator_dw9714_imx091.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714_imx219_475t_camcorder.so:system/vendor/lib/libactuator_dw9714_imx219_475t_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714_imx219_475t_camera.so:system/vendor/lib/libactuator_dw9714_imx219_475t_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714_imx219_475t.so:system/vendor/lib/libactuator_dw9714_imx219_475t.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9714.so:system/vendor/lib/libactuator_dw9714.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9716_camcorder.so:system/vendor/lib/libactuator_dw9716_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9716_camera.so:system/vendor/lib/libactuator_dw9716_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9716.so:system/vendor/lib/libactuator_dw9716.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_camcorder.so:system/vendor/lib/libactuator_dw9718_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_camera.so:system/vendor/lib/libactuator_dw9718_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_imx179_camcorder.so:system/vendor/lib/libactuator_dw9718_imx179_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_imx179_camera.so:system/vendor/lib/libactuator_dw9718_imx179_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_imx179.so:system/vendor/lib/libactuator_dw9718_imx179.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_imx219_495t_camcorder.so:system/vendor/lib/libactuator_dw9718_imx219_495t_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_imx219_495t_camera.so:system/vendor/lib/libactuator_dw9718_imx219_495t_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_imx219_495t.so:system/vendor/lib/libactuator_dw9718_imx219_495t.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_ov8858_camcorder.so:system/vendor/lib/libactuator_dw9718_ov8858_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_ov8858_camera.so:system/vendor/lib/libactuator_dw9718_ov8858_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718_ov8858.so:system/vendor/lib/libactuator_dw9718_ov8858.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_dw9718.so:system/vendor/lib/libactuator_dw9718.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_iu074_camcorder.so:system/vendor/lib/libactuator_iu074_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_iu074_camera.so:system/vendor/lib/libactuator_iu074_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_iu074.so:system/vendor/lib/libactuator_iu074.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_ov12830_camcorder.so:system/vendor/lib/libactuator_ov12830_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_ov12830_camera.so:system/vendor/lib/libactuator_ov12830_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_ov12830.so:system/vendor/lib/libactuator_ov12830.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_ov8825_camcorder.so:system/vendor/lib/libactuator_ov8825_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_ov8825_camera.so:system/vendor/lib/libactuator_ov8825_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_ov8825.so:system/vendor/lib/libactuator_ov8825.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_rohm_bu64243gwz_camcorder.so:system/vendor/lib/libactuator_rohm_bu64243gwz_camcorder.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_rohm_bu64243gwz_camera.so:system/vendor/lib/libactuator_rohm_bu64243gwz_camera.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libactuator_rohm_bu64243gwz.so:system/vendor/lib/libactuator_rohm_bu64243gwz.so \
-    vendor/lge/f70n/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libmmjpeg.so:system/vendor/lib/libmmjpeg.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libjpegdhw.so:system/vendor/lib/libjpegdhw.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libjpegehw.so:system/vendor/lib/libjpegehw.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libqomx_jpegdec.so:system/vendor/lib/libqomx_jpegdec.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libqomx_jpegenc.so:system/vendor/lib/libqomx_jpegenc.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libmmqjpeg_codec.so:system/vendor/lib/libmmqjpeg_codec.so \
+    vendor/lge/f70n/proprietary/vendor/lib/libmm-hdcpmgr.so:system/vendor/lib/libmm-hdcpmgr.so \
     vendor/lge/f70n/proprietary/lib/libdashplayer.so:system/lib/libdashplayer.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmosal.so:system/vendor/lib/libmmosal.so \
     vendor/lge/f70n/proprietary/vendor/lib/libmmparser.so:system/vendor/lib/libmmparser.so \
@@ -280,11 +258,9 @@ PRODUCT_COPY_FILES += \
     vendor/lge/f70n/proprietary/vendor/lib/hw/sensors.msm8226.so:system/vendor/lib/hw/sensors.msm8226.so \
     vendor/lge/f70n/proprietary/vendor/lib/libsensor1.so:system/vendor/lib/libsensor1.so \
     vendor/lge/f70n/proprietary/vendor/lib/libsensor_reg.so:system/vendor/lib/libsensor_reg.so \
-    vendor/lge/f70n/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
-    vendor/lge/f70n/proprietary/vendor/lib/libsensor_test.so:system/vendor/lib/libsensor_test.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libsensor_thresh.so:system/vendor/lib/libsensor_thresh.so \
     vendor/lge/f70n/proprietary/vendor/lib/libsensor_user_cal.so:system/vendor/lib/libsensor_user_cal.so \
-    vendor/lge/f70n/proprietary/vendor/lib/libsensor_lge_cal.so:system/vendor/lib/libsensor_lge_cal.so \
+    vendor/lge/f70n/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
+    vendor/lge/f70n/proprietary/vendor/lib/libbmc050.so:system/vendor/lib/libbmc050.so \
     vendor/lge/f70n/proprietary/bin/mpdecision:system/bin/mpdecision \
     vendor/lge/f70n/proprietary/lib/libwebviewchromium_loader.so:system/lib/libwebviewchromium_loader.so \
     vendor/lge/f70n/proprietary/lib/libwebviewchromium_plat_support.so:system/lib/libwebviewchromium_plat_support.so \
